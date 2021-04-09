35dcb6ca332e0b3e8ba9f46559adcc223720dc7f
Pak Tani <johansantri@live.com>
2021-04-09 14:30:18 +0700

    update dasbor card
    card dack

--- application/views/dasbor.php
+++ application/views/dasbor.php
@@ -16,47 +16,110 @@
 
         <!-- DataTables -->
         <div class="card mb-3">
-          <div class="card-header">
-            <a href="<?php echo site_url('admin/personal/add') ?>"><i class="fas fa-plus"></i> Add New</a>
-          </div>
-          <div class="card-body">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <a class="navbar-brand" href="#"><img src="<?php echo base_url()?>upload/iceeeee.jpg" width="30" height="30" alt="" >&nbsp; &nbsp;ICE-I</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-            <div class="table-responsive">
-              <table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Phone</th>
-                    <th>Photo</th>
-                    <th>Description</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php foreach ($Personal as $person): ?>
-                  <tr>
-                    <td width="150">
-                      <?php echo $person->name ?>
-                    </td>
-                    <td>
-                      <?php echo $person->no_hp ?>
-                    </td>
-                    <td>
-                      <img src="<?php echo base_url('upload/personal/'.$person->image) ?>" width="64" />
-                    </td>
-                    <td class="small">
-                      <?php echo substr($person->tentang, 0, 120) ?>...</td>
-                    <td width="250">
-                     <a href="#">x</a>
-                    </td>
-                  </tr>
-                  <?php endforeach; ?>
-
-                </tbody>
-              </table>
-            </div>
-          </div>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Biografi</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#"></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<hr>
+<br>
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+<br>
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+<br>
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+<div class="container">
+<div class="row">
+<?php foreach ($Personal as $person): ?>
+    <div class="col-md-4">
+       
+  <div class="card-deck">
+    
+  <div class="card border-primary">
+    <div class="row">
+      <div class="col-md-4">
+          <img class="card-img-top" src="<?php echo base_url('upload/personal/'.$person->image) ?>" style=" border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 140px; height: 150px" alt="Card image cap">
+      </div>
+  <div class="col-md-8">
+      <div class="card-body">
+      <h6 class="card-title"><?php echo $person->name ?></h6>
+      <p class="card-text"><small><?php echo substr($person->tentang, 0, 120) ?></small></p>
+    </div>
+    </div>
+ 
+  
+  
+  </div>
+     <div class="card-footer" style="text-align: center;">
+      <small class="text-muted" ><?php echo $person->no_hp ?> || <?php echo $person->email ?> || <?php echo $person->alamat ?></small>
+    </div>
+ </div>
+ 
+</div>
+ 
+</div>
+   <?php endforeach; ?>
+ 
+</div>
+</div>
+
+
+   
+   
+   
+</div>
+
+
+
+
+
+
+
+
+
 <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
